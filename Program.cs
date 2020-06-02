ddad6b5bf7c97588bf48149bb92716d0563f19ec
Rob Mack <robmack8932@icloud.com>
2020-06-02 09:12:09 -0400

    BJack

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
-namespace BJack
+namespace Blackjack
 {
-  class Program
-  {
-    static void Main(string[] args)
+
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var keepPlaying = true;
+
+            while (keepPlaying)
+            {
+                var game = new Game();
+                game.Play();
+
+                // 21. Ask the user if they want to play again.
+                Console.Write("Play again? (Y/N): ");
+                var playAgainString = Console.ReadLine();
+
+                // if (playAgainString == "Y")
+                // {
+                //     keepPlaying = true;
+                // }
+                // else
+                // {
+                //     keepPlaying = false;
+                // }
+
+                // This one line is equivalent to the above lines
+                keepPlaying = (playAgainString == "Y");
+            }
+
+            // 22. Show the user "Thanks for playing"
+            Console.WriteLine();
+            Console.WriteLine("Thanks for playing");
+        }
     }
-  }
 }
